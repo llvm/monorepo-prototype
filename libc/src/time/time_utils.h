268e870dca1bfb7819b1a397228be63008b2828b
Зишан Мирза <zmirza@tutanota.de>
2024-10-06 21:18:10 +0200

    fix: daylight saving time

--- libc/src/time/time_utils.h
+++ libc/src/time/time_utils.h
@@ -98,8 +98,6 @@ LIBC_INLINE struct tm *localtime(const time_t *t_ptr) {
     return nullptr;
   }
 
-  set_dst(&result);
-
   return &result;
 }
 
@@ -112,8 +110,6 @@ LIBC_INLINE struct tm *localtime_internal(const time_t *t_ptr,
     out_of_range();
     return nullptr;
   }
-
-  set_dst(input);
 
   return input;
 }
