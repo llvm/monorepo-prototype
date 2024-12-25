eb30ca064969696f2d020d27ab1e54a53ca4b85c
Jacek Caban <jacek@codeweavers.com>
2024-12-25 23:37:00 +0100

    [LLD][COFF] Separate EC and native exports for ARM64X
    Store exports in SymbolTable instead of Configuration.

--- lld/COFF/Driver.h
+++ lld/COFF/Driver.h
@@ -182,7 +182,6 @@ private:
   std::list<std::function<void()>> taskQueue;
   std::vector<MemoryBufferRef> resources;
 
-  llvm::DenseSet<StringRef> directivesExports;
   llvm::DenseSet<StringRef> excludedSymbols;
 
   COFFLinkerContext &ctx;
@@ -246,8 +245,6 @@ private:
 
   // Used for dllexported symbols.
   Export parseExport(StringRef arg);
-  void fixupExports();
-  void assignExportOrdinals();
 
   // Parses a string in the form of "key=value" and check
   // if value matches previous values for the key.
