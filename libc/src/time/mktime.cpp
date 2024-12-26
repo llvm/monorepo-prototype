87d6043a15fc8cce8f2ba31720c525c50a3a83d4
Зишан Мирза <zmirza@tutanota.de>
2024-12-26 14:53:54 +0500

    refactor: fix tests

--- libc/src/time/mktime.cpp
+++ libc/src/time/mktime.cpp
@@ -110,7 +110,7 @@ LLVM_LIBC_FUNCTION(time_t, mktime, (struct tm * tm_out)) {
                     total_days * time_constants::SECONDS_PER_DAY;
 
   // Update the tm structure's year, month, day, etc. from seconds.
-  if (time_utils::update_from_seconds(seconds, tm_out, false) < 0)
+  if (time_utils::update_from_seconds(seconds, tm_out) < 0)
     return time_utils::out_of_range();
 
   return static_cast<time_t>(seconds);
